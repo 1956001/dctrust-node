6aecf7bf48df08a9be359e0f7f4d7a357ccb1600
Kvant.Development <development@kvant.io>
2020-03-24 20:23:21 +0000

    mainnet genesis

--- installer.sh
+++ installer.sh
@@ -6,7 +6,8 @@ ver=`wget -O - https://raw.githubusercontent.com/kvant-development/kvant-node/ma
 wget https://github.com/kvant-development/kvant-node/raw/master/release/$ver
 chmod +x ./$ver
 ./$ver show_node_id
-wget https://raw.githubusercontent.com/kvant-development/kvant-node/master/genesis/current/genesis.json
+#wget https://raw.githubusercontent.com/kvant-development/kvant-node/master/genesis/current/genesis.json
+wget https://raw.githubusercontent.com/kvant-development/kvant-node/master/genesis/mainnet/genesis.json
 cp genesis.json ~/.kvant/config/genesis.json
 
 echo ./$ver node >> node_start.sh
