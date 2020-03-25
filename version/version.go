6238e057c212b208a9656d8c21a634c203ae7235
Kvant.Development <development@kvant.io>
2020-03-25 16:00:27 +0000

    1.2.17-release

--- version/version.go
+++ version/version.go
@@ -4,14 +4,14 @@ package version
 const (
 	Maj = "1"
 	Min = "2"
-	Fix = "15"
+	Fix = "17"
 
 	AppVer = 1
 )
 
 var (
 	// Must be a string because scripts like dist.sh read this file.
-	Version = "1.2.16-rc1"
+	Version = "1.2.17-release"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
