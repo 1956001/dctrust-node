2d2c1e6daf7dd85194c8d85f93b37e1b719adbc8
1956001 <72578509+1956001@users.noreply.github.com>
2020-10-17 20:41:20 +0300

    Update dao.go

--- core/dao/dao.go
+++ core/dao/dao.go
@@ -3,6 +3,6 @@ package dao
 import "github.com/kvant-node/core/types"
 
 var (
-	Address    = types.HexToAddress("Mx18467bbb64a8edf890201d526c35957d82be3d95")
+	Address    = types.HexToAddress("Kx958f94ed33df095bb3ef07627af084f23cfe4f5e")
 	Commission = 0
 )
